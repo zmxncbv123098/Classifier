7e6a1b88f4b45dd85cb966d24c1b49606f126232
zmxncbv123098 <miketwin1@mail.ru>
2020-10-14 14:45:39 +0300

    reformat file structure

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
 from dataset import CustomDataset
+from net import Net
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
 
 
 dataset = CustomDataset('')
@@ -67,28 +47,3 @@ print('Finished Training')
 """ Save model """
 PATH = './cifar_net.pth'
 torch.save(net.state_dict(), PATH)
-
-# --- #
-
-dataiter = iter(data_loader)
-images, labels = dataiter.next()
-print("Truth" + ' '.join('%5s' % inverted_labels[int(labels[j])] for j in range(4)))
-
-net = Net()
-net.load_state_dict(torch.load(PATH))
-outputs = net(images)
-_, predicted = torch.max(outputs, 1)
-print("Predicted" + ' '.join('%5s' % inverted_labels[int(predicted[j])] for j in range(4)))
-
-correct = 0
-total = 0
-with torch.no_grad():
-    for data in data_loader:
-        images, labels = data
-        outputs = net(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-
-print('Accuracy of the network on the 10000 test images: %d %%' % (
-        100 * correct / total))
