8ce9fca850afd9e8d86e9610aea9af505ee91d87
zmxncbv123098 <miketwin1@mail.ru>
2020-10-15 11:50:14 +0300

    move test_model.py to root

---
+++ test_model.py
@@ -0,0 +1,44 @@
+from dataset import CustomDataset
+from model import Net
+import numpy as np
+import torch
+from torch.utils.data import SubsetRandomSampler
+
+
+def test_model(PATH, dataset, batch_size, sampler):
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size, sampler=sampler)
+    net = Net()
+    net.load_state_dict(torch.load(PATH))
+
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in data_loader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the %s val images: %d %%' % (len(sampler),
+                                                                   100 * correct / total))
+
+
+dataset = CustomDataset('')
+
+batch_size = 4
+validation_split = .2
+shuffle_dataset = True
+random_seed = 42
+
+dataset_size = len(dataset)
+indices = list(range(dataset_size))
+split = int(np.floor(validation_split * dataset_size))
+if shuffle_dataset:
+    np.random.seed(random_seed)
+    np.random.shuffle(indices)
+train_indices, val_indices = indices[split:], indices[:split]
+
+valid_sampler = SubsetRandomSampler(val_indices)
+
+test_model("cifar_net.pth", dataset, batch_size, valid_sampler)
